9e56443cc410deeed30f67dd3350b4e8ceac33dc
thishantj <thishantj@gmail.com>
2021-07-02 15:00:27 +0530

    IMP update

---
+++ lib/api/image_processing_api.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:google_ml_kit/google_ml_kit.dart';
+// import 'package:firebase_ml_vision/firebase_ml_vision.dart';
+
+class ImageProcessingApi {
+  static Future<String> recogniseText(File imageFile) async {
+    if (imageFile == null) {
+      return "No selected image";
+    } else {
+      // final visionImage = FirebaseVisionImage.fromFile(imageFile);
+      // final textRecognizer = FirebaseVision.instance.textRecognizer();
+
+      // try {
+      //   final visionText = await textRecognizer.processImage(visionImage);
+      //   await textRecognizer.close();
+
+      //   final text = extractText(visionText);
+      //   return text.isEmpty ? 'No text found in the image' : text;
+      // } catch (error) {
+      //   return error.toString();
+      // }
+
+
+      // final visionImage = InputImage.fromFile(imageFile);
+      final visionImage = InputImage.fromFile(imageFile);
+      final textRecognizer = GoogleMlKit.vision.textDetector();
+
+      try {
+        final visionText = await textRecognizer.processImage(visionImage);
+        await textRecognizer.close();
+
+        final text = extractText(visionText);
+        return text.isEmpty ? 'No text found in the image' : text;
+      } catch (error) {
+        return error.toString();
+      }
+    }
+  }
+
+  // static Future<String> recogniseText(WriteBuffer bytes, InputImageData inputImageData) async {
+  //   if (inputImageData == null) {
+  //     return "No selected image";
+  //   } else {
+  //     final visionImage = InputImage.fromBytes(bytes: bytes.done().buffer.asUint8List(), inputImageData: inputImageData);
+  //     final textRecognizer = GoogleMlKit.vision.textDetector();
+
+  //     try {
+  //       final visionText = await textRecognizer.processImage(visionImage);
+  //       await textRecognizer.close();
+
+  //       final text = extractText(visionText);
+  //       return text.isEmpty ? 'No text found in the image' : text;
+  //     } catch (error) {
+  //       return error.toString();
+  //     }
+  //   }
+  // }
+
+  static extractText(RecognisedText visionText) {
+    String text = '';
+
+    for (TextBlock block in visionText.blocks) {
+      for (TextLine line in block.lines) {
+        for (TextElement word in line.elements) {
+          text = text + word.text + ' ';
+        }
+        text = text + '\n';
+      }
+    }
+
+    return text;
+  }
+}
